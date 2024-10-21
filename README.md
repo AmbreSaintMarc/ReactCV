726414b7d020e8d9feef84196ed32aeb8ee53edc
Ambre <saintmarc.ambre@gmail.com>
2024-10-22 00:50:29 +0200

    Ajout des images du dossier, de la partie presentation de l'accueil et de fichier css

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 You'll need to make sure you have the latest react version
 
-You need to donload react router with the command : npm install react-router-dom
+You need to download : 
+- react router with the command : npm install react-router-dom
+- the dependency react-anchor-link-smooth-scroll : npm i react-anchor-link-smooth-scroll
--- src/Accueil.jsx
+++ src/Accueil.jsx
@@ -1,17 +1,29 @@
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import "./general.css"
+import "./accueil.css"
+
 
 
 export default function Accueil() {
     return (
       <div className="Accueil">
         <body>
-        <section class="presentation">
-          <img src="" alt="Image d'un développeur devant son ordinateur"></img>
-          <h1>Bonjour, je suis John Doe</h1>
-          <h2>Développeur web full stack</h2>
-          <button>En savoir plus</button>
+        <section className="presentation">
+            <div className="col-md-12">
+                <div id="main-img">
+                    <div className="layer"></div>
+                    <section className="presentation-text">
+                    <h1 className="presentation-text__title">Bonjour, je suis John Doe</h1>
+                    <h2 className="presentation-text__sub-text">Développeur web full stack</h2>
+                    <AnchorLink href="#About">
+                    <button className="presentation-text__button">En savoir plus</button>
+                    </AnchorLink>
+                </section>
+                </div>
+            </div>
         </section>
 
-        <section class="about">
+        <section id="About">
             <h3>A propos</h3>
             <p>Lorem Ipsum</p>
             <img src="" alt ="Image d'un homme concentré"></img>
@@ -19,7 +31,7 @@ export default function Accueil() {
         </section>
         </body>
 
-      </div>
+        </div>
     );
   }
   
